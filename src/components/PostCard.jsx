c8a03833b9eb5252102af6adbd8844e3424ec97d
Jitendra Rewar <jiturewar9@gmail.com>
2026-02-08 06:37:19 +0530

    change css

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,30 +1,3 @@
-// import React from "react";
-// import appwriteService from "../appwrite/config";
-
-// function PostCard({ title, featuredimage }) {
-//   const imageUrl = featuredimage
-//     ? appwriteService.getFileView(featuredimage)
-//     : "/placeholder.png";
-
-//   return (
-//     <div className="w-full bg-white rounded-xl overflow-hidden shadow hover:shadow-lg transition">
-//       <img
-//         src={imageUrl}
-//         alt={title}
-//         className="w-full h-48 object-cover"
-//         onError={(e) => {
-//           e.currentTarget.src = "/placeholder.png";
-//         }}
-//       />
-//       <h2 className="p-4 text-gray-900 font-semibold text-lg">
-//         {title}
-//       </h2>
-//     </div>
-//   );
-// }
-
-// export default PostCard;
-
 import React from "react";
 import { Link } from "react-router-dom";
 import appwriteService from "../appwrite/config";
